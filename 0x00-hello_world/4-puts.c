5a26332932ee5838839e3f51a95772b36dc01295
Shadylhawary <shadylhawary@gmail.com>
2023-06-15 14:04:55 -0700

    Fixing errors

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
-/*Function Declartion*/
+/*
+ *  main - A program that prints a line with puts function
+ *  return: 0 
+ *  */
 #include <stdio.h>
 int main(void)
 {
-puts("\"Programming is like building a multilinugual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
